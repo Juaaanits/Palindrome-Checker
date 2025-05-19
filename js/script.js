5e1f5fc61dd5bbc64e949b62f4b9c3b811956f2f
Juaaanits <juanitoramos113@gmail.com>
2025-05-19 12:12:56 +0800

    Revised javascript

--- js/script.js
+++ js/script.js
@@ -1,4 +1,28 @@
-//Function to check if a number or string is palindrome
+const input = document.querySelector('#text-input');
+const checkBtn = document.querySelector('#check-btn');
+const result = document.querySelector('#result');
+
+checkBtn.addEventListener('click', () => {
+    const originalText = input.value;
+    if (!originalText){
+        alert("Please input a value");
+        return;
+    }
+
+    const cleanedText = originalText.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    const reversedText = cleanedText.split('').reverse().join('');
+
+    if (cleanedText === reversedText) {
+        result.textContent = `${originalText} is a palindrome`;
+    } else {
+        result.textContent = `${originalText} is not a palindrome`;
+    }
+
+
+});
+
+
+/*Function to check if a number or string is palindrome
 function isPalindrome(input) {
     // Convert the input to a string
     const str = input.toString();
@@ -23,3 +47,5 @@ if (isPalindrome(input)) {
 else {
     console.log("no");
 }
+
+*/
