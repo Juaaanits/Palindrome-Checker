f168c245535e9772fa27afa27b8f69f6e3150281
Juaaanits <juanitoramos113@gmail.com>
2025-05-19 17:10:26 +0800

    Finalized JS and CSS

--- js/script.js
+++ js/script.js
@@ -12,40 +12,12 @@ checkBtn.addEventListener('click', () => {
     const cleanedText = originalText.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
     const reversedText = cleanedText.split('').reverse().join('');
 
+    result.classList.remove('hidden');
+
     if (cleanedText === reversedText) {
         result.textContent = `${originalText} is a palindrome`;
     } else {
         result.textContent = `${originalText} is not a palindrome`;
     }
 
-
 });
-
-
-/*Function to check if a number or string is palindrome
-function isPalindrome(input) {
-    // Convert the input to a string
-    const str = input.toString();
-    // Get the length of the string
-    const len = str.length;
-    // Loop through half of the string
-    for (let i = 0; i < len / 2; i++) {
-        // Check if the characters at the start and end are different
-        if (str[i] !== str[len - 1 - i]) {
-            return false; // Not a palindrome
-        }
-    }
-    return true; // Is a palindrome
-}
-
-
-let input = 12321;
-//If palindrom, output yes else no
-if (isPalindrome(input)) {
-    console.log("yes");
-}   
-else {
-    console.log("no");
-}
-
-*/
