cebd031094c6b68f06b4334480e909462618edea
Juaaanits <juanitoramos113@gmail.com>
2025-05-20 22:39:24 +0800

    Added dark/light mode button

--- js/script.js
+++ js/script.js
@@ -1,23 +1,36 @@
 const input = document.querySelector('#text-input');
 const checkBtn = document.querySelector('#check-btn');
 const result = document.querySelector('#result');
+const clrButton = document.querySelector('#clear-btn');
+const toggleMode = document.getElementById('toggle-mode');
 
 checkBtn.addEventListener('click', () => {
-    const originalText = input.value;
-    if (!originalText){
-        alert("Please input a value");
-        return;
-    }
+  const originalText = input.value;
 
-    const cleanedText = originalText.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
-    const reversedText = cleanedText.split('').reverse().join('');
+  if (!originalText) {
+    alert("Please input a value");
+    return;
+  }
 
-    result.classList.remove('hidden');
+  const cleanedText = originalText.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  const reversedText = cleanedText.split('').reverse().join('');
 
-    if (cleanedText === reversedText) {
-        result.textContent = `${originalText} is a palindrome`;
-    } else {
-        result.textContent = `${originalText} is not a palindrome`;
-    }
+  result.classList.remove('hidden');
 
+  if (cleanedText === reversedText) {
+    result.textContent = `"${originalText}" is a palindrome`;
+  } else {
+    result.textContent = `"${originalText}" is not a palindrome`;
+  }
+});
+
+clrButton.addEventListener('click', () => {
+  input.value = '';
+  result.textContent = '';
+  result.classList.add('hidden');
+  input.focus();
+});
+
+toggleMode.addEventListener('change', () => {
+  document.body.classList.toggle('dark-mode');
 });
